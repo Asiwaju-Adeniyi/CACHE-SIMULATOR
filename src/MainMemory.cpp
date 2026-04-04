c933fae2bfe125b06d864c3448f033779ddba19f
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-04-04 03:48:06 +0100

    Read, Write, and Print Member Functions

--- src/MainMemory.cpp
+++ src/MainMemory.cpp
@@ -1,7 +1,8 @@
 #include "MainMemory.h"
 #include <array> 
-
-
+#include <iostream> 
+#include <string> 
+#include <format> 
    
     MainMem::MainMem(){
         memory = std::make_unique<std::array<uint8_t, MAIN_MEM_SIZE>>();
@@ -16,6 +17,25 @@
     void MainMem::Write(uint32_t startAdd, uint8_t size, uint8_t* dst){
         std::memcpy(&memory.get()->at(startAdd), dst, size);
     };
-    void MainMem::Print(){};
+    void MainMem::Print(){
+        const uint32_t rows = 24;
+        const uint8_t cols = 12;
+
+        for (uint32_t row = 0; row < rows; row++) {
+            for (uint8_t col = 0; col < cols; col++) {
+              std::string val = std::format("0x{:x}/t", memory.get()->at(row * cols + col));
+
+              std::cout << val;
+            }
+              std::cout << std::endl;
+        }
+
+            std::cout << std::endl;
+            for (uint32_t dash = 0; dash < 80; dash++) {
+                std::cout << "-";
+            }
+
+            std::cout << std::endl;
+    };
 
 
